b4fd8870354285a6a7d65e7a8c801418836eedaa
root <root@debian8svm.debian8svm>
2016-11-07 16:33:41 +0100

    Changes for dev-mode

--- php7-fpm/loadFixtures.sh
+++ php7-fpm/loadFixtures.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-/var/www/symfony/bin/console doctrine:schema:create
-/var/www/symfony/bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Hanoi/ --append
-/var/www/symfony/bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/RioPrimero/ --append
+composer install
+composer update
+
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+
+/var/www/inowas/bin/console doctrine:schema:create
+/var/www/inowas/bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/Hanoi/ --append
+/var/www/inowas/bin/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORM/Scenarios/RioPrimero/ --append
